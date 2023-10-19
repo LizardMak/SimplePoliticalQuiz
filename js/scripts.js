8575098dd7d97cf61c4fae397cb2dcd38e361446
Hunter Empey <LizardMak@gmail.com>
2023-10-18 18:14:22 -0600

    add all parties

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   form = document.querySelector('form');
-    form.onsubmit = function(e) {
+  form.onsubmit = function (e) {
     e.preventDefault();
     console.log("form submitted!");
     let inputGun = parseInt(document.getElementById('inputGun').value);
@@ -20,19 +20,19 @@ window.onload = function () {
     let ukraineError = 0;
 
     if (inputGun === 1) {
-       gunRightAdd = 1;
-    } else if (inputGun === 2 || 3 || 4) {
-       gunModAdd = 1;
+      gunRightAdd = 1;
+    } else if (inputGun === 2 || inputGun === 3 || inputGun === 4) {
+      gunModAdd = 1;
     } else if (inputGun === 5) {
-       gunLeftAdd = 1;
+      gunLeftAdd = 1;
     } else if (inputGun > 5) {
-       gunError = 100;
+      gunError = 100;
     } else if (inputGun < 0) {
-       gunError = 100;
+      gunError = 100;
     };
     if (inputHealth === 1) {
       healthRightAdd = 1;
-    } else if (inputHealth === 2 || 3 || 4) {
+    } else if (inputHealth === 2 || inputHealth === 3 || inputHealth === 4) {
       healthModAdd = 1;
     } else if (inputHealth === 5) {
       healthLeftAdd = 1;
@@ -43,7 +43,7 @@ window.onload = function () {
     };
     if (inputUkraine === 1) {
       ukraineRightAdd = 1;
-    } else if (inputUkraine === 2 || 3 || 4) {
+    } else if (inputUkraine === 2 || inputUkraine === 3 || inputUkraine === 4) {
       ukraineModAdd = 1;
     } else if (inputUkraine === 5) {
       ukraineLeftAdd = 1;
@@ -52,19 +52,28 @@ window.onload = function () {
     } else if (inputUkraine < 0) {
       ukraineError = 100;
     };
-    console.log("finished");
+  
+  console.log("finished");
 
-    let left = gunLeftAdd + healthLeftAdd + ukraineLeftAdd;
-    let right = gunRightAdd + healthRightAdd + ukraineRightAdd;
-    let moderate = gunModAdd + healthModAdd + ukraineModAdd;
-    let errorCheck = gunError + healthError + ukraineError;
-    console.log(left);
-    console.log(right);
-    console.log(moderate);
-    console.log(errorCheck);
-    if (right > left && right > moderate && right > errorCheck) {
-      let resultRight = document.getElementById('resultRight');
-      resultRight.removeAttribute('class');
-    }
-    };
+  let left = gunLeftAdd + healthLeftAdd + ukraineLeftAdd;
+  let right = gunRightAdd + healthRightAdd + ukraineRightAdd;
+  let moderate = gunModAdd + healthModAdd + ukraineModAdd;
+  let errorCheck = gunError + healthError + ukraineError;
+  console.log(left);
+  console.log(right);
+  console.log(moderate);
+  console.log(errorCheck);
+  if (right > left && right > moderate && right > errorCheck) {
+    let resultRight = document.getElementById('resultRight');
+    resultRight.removeAttribute('class');
+  } else if (left > right && left > moderate && left > errorCheck) {
+    let resultLeft = document.getElementById('resultLeft');
+    resultLeft.removeAttribute('class');
+  } else if (moderate > left && moderate > right && moderate > errorCheck || moderate === left && moderate === right && left === right) {
+    resultModerate.removeAttribute('class');
+  } else if (errorCheck > left) {
+    let error = document.getElementById('error');
+    error.removeAttribute('class');
   }
+}
+}
